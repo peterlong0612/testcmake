a7d1869968c1260456887e9d1863951afa3339f3
peterlong0612 <41103069+peterlong0612@users.noreply.github.com>
2019-12-19 22:54:05 +0800

    select image

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,6 +24,7 @@ import android.widget.TextView;
 //import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,12 +33,35 @@ import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
+    public String TAG="MainActivity";
+    //private static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private ImageView imgViewA;
+    private ImageView imgViewB;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PICK_IMAGE = 1;
+    //private static final int PICK_VIDEO = 2;
+    String strdecode="sdcard/rp.png";
+
+    public Uri imgUri;
+
+    private Button mBtn;
 
     private ImageView iv;//白平衡
     private ImageView lu;//亮度提升
     private ImageView re;//红眼
     private ImageView oof;//失焦
     private ImageView rp;//图像修复
+
+    private Button btn_bph;
+    private Button btn_lu;
+    private Button btn_rp;
+    private Button btn_re;
+    private Button btn_main_select;
+
+
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -41,23 +72,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initBtn();
+        initView();
         //Button button2 = (Button) findViewById(R.id.button2);
         //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background);
 
         // Example of a call to a native method
 
         //tv.setText(stringFromJNI());
-        iv = findViewById(R.id.imageView7);
-        re = findViewById(R.id.redeye);
-        lu = findViewById(R.id.lightup);
+        //iv = findViewById(R.id.imageView7);
+        //re = findViewById(R.id.redeye);
+        //lu = findViewById(R.id.lightup);
         //oof = findViewById(R.id.oof);
-        rp = findViewById(R.id.rp);
+        //rp = findViewById(R.id.rp);
+
         //matrixFromJNI(bitmap);
         PermissionUtils.isGrantExternalRW(this,1);
 
     }
 
+    private void initBtn(){
+        btn_bph =  findViewById(R.id.btn_bph);
+        btn_lu =  findViewById(R.id.btn_lu);
+        btn_rp = findViewById(R.id.btn_rp);
+        btn_re = findViewById(R.id.btn_re);
+        btn_main_select = findViewById(R.id.btn_main_select);
+    }
+    private void initView(){
+        imgViewA = findViewById(R.id.imgViewA);
+        imgViewB = findViewById(R.id.imgViewB);
 
+    }
+
+    /*
+    * 打开本地相册选择图片
+    * */
+    public void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
+                PICK_IMAGE);
+    }
 
     public native void matrixFromJNI(Object bitmap);//白平衡实现函数
     public native void repair(Object bitmap);
@@ -67,7 +123,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void OnClick(View v){
         switch (v.getId()){
-            case R.id.rp_btn:
+            case R.id.btn_main_select:
+            {
+                chooseImage();
+                break;
+            }
+
+            case R.id.btn_rp:
             {
                 //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.rp);//drawable.rp为需要的图片
                 String strdecode="sdcard/rp.png";
@@ -82,9 +144,11 @@ public class MainActivity extends AppCompatActivity {
 
                 strdecode="sdcard/rpoutput.jpg";
                 Bitmap bitmap1 = BitmapFactory.decodeFile(strdecode);
-                rp.setImageBitmap(bitmap1);
+                //rp.setImageBitmap(bitmap1);
+                imgViewB.setImageBitmap(bitmap1);
+                break;
             }
-            case R.id.bph_btn :
+            case R.id.btn_bph:
             {
                 TextView tv = findViewById(R.id.sample_text);
                 tv.setText("changed");//查看图片是否改变，用于初期实验用，可删除
@@ -92,10 +156,11 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("白平衡");
                 System.out.println(bitmap);
                 matrixFromJNI(bitmap);
-                iv.setImageBitmap(bitmap);
+                //iv.setImageBitmap(bitmap);
+                imgViewB.setImageBitmap(bitmap);
                 break;
             }
-            case R.id.lu_btn:
+            case R.id.btn_lu:
             {
                 Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.lu);//drawable.lu为需要的图片
 
@@ -103,10 +168,11 @@ public class MainActivity extends AppCompatActivity {
 
                 System.out.println(bitmap);
                 lightup(bitmap);
-                lu.setImageBitmap(bitmap);
-
+                //lu.setImageBitmap(bitmap);
+                imgViewB.setImageBitmap(bitmap);
+                break;
             }
-            case R.id.re_btn:
+            case R.id.btn_re:
             {
                 //Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.redeye);//drawable.lu为需要的图片
                 Bitmap bitmap = BitmapFactory.decodeFile("sdcard/redeyepic.jpg");
@@ -132,16 +198,45 @@ public class MainActivity extends AppCompatActivity {
 
                     redeye(bitmap,mCascadeFile.getAbsolutePath());
                     Bitmap bitmap1 = BitmapFactory.decodeFile("sdcard/output.jpg");
-                    re.setImageBitmap(bitmap1);
-
+                    //re.setImageBitmap(bitmap1);
+                    imgViewB.setImageBitmap(bitmap1);
                     cascadeDir.delete();
                 } catch (IOException e){
                     e.printStackTrace();
                     System.out.println( "Failed to load cascade. Exception thrown: " + e);
                 }
+                break;
             }
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        //用户操作完成，结果代码返回是-1，即RERULT_OK
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //todo 获取文件定位，放置选择的图片
+            imgUri= data.getData();
+            Log.e("imgUri", imgUri.toString());
+
+            ContentResolver cr= this.getContentResolver();
+            try{
+              //获取图片
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(imgUri));
+                imgViewA.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e){
+                Log.e("Exception",e.getMessage(),e);
+            }
+            //imgViewA.setImageURI(imgUri);
+            //videoView.start();  //播放视频
+        }
+        else{
+            //操作错误或者没有选择图片
+            Log.i("MainActivity","Operation Error!");
+
+        }
+        Log.d(TAG, "onActivityResult() called with: requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "]");
     }
 
     /*public native void outoffocus(Object bitmap);
@@ -227,5 +322,29 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_IMAGE_CAPTURE: {
+                //todo 判断权限是否已经授予
+
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                for (int i = 0; i < permissions.length ; i++ ){
+                    Log.i("MainActivity","申请的权限为：" + permissions[i] +"，申请结果：" +
+                            grantResults[i]);
+                }
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                    //takeVideo();
+                    break;
+                }
+
+
+            }
+        }
+    }
 
 }
